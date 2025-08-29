ce9c3b92709962a0d5d0ad1c0783e49aabc20872
ManavGaur741 <manavgaur741@gmail.com>
2025-08-30 03:56:26 +0530

    Update server.js

--- CodeXcape/server.js
+++ CodeXcape/server.js
@@ -6,6 +6,11 @@ const port = process.env.PORT || 3000;
 // Set the static directory to 'codexcape'
 app.use(express.static(path.join(__dirname, 'CodeXcape')));
 
+// Serve login.html for the root URL
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'CodeXcape', 'login.html'));
+});
+
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
