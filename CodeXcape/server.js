121b5f7a49f1b225509242742fb290d47dac5413
ManavGaur741 <manavgaur741@gmail.com>
2025-08-30 03:51:14 +0530

    Update server.js

--- CodeXcape/server.js
+++ CodeXcape/server.js
@@ -3,7 +3,8 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname)));
+// Set the static directory to 'codexcape'
+app.use(express.static(path.join(__dirname, 'CodeXcape')));
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
